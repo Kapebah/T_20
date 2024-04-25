5bd3b7e5e60ab2d4250e950873f8281cf632de2c
Kapebah <sh1n0sh1@mail.ru>
2024-04-25 15:02:02 +0300

    hometask

---
+++ T_20 (Tasks)/Hometask/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hometask
+{
+    class Calculator<T, K>
+    {
+        public T Value1 { get; set; }
+        public K Value2 { get; set; }
+        public Calculator() { }
+        public Calculator(T value1, K value2) {  Value1 = value1; Value2 = value2; }
+        public void Add()
+        {
+            if (typeof(T) == typeof(int) || typeof(K) == typeof(int) || typeof(T) == typeof(double) || typeof(K) == typeof(double) || typeof(T) == typeof(string) || typeof(K) == typeof(string))
+            {
+                double a = Convert.ToDouble(Value1);
+                double b = Convert.ToDouble(Value2);
+                Console.WriteLine($"{Value1} + {Value2} = {a + b}");
+            }
+            else { Console.WriteLine("Неверный тип"); }
+        }
+        public void Substract()
+        {
+            if (typeof(T) == typeof(int) || typeof(K) == typeof(int) || typeof(T) == typeof(double) || typeof(K) == typeof(double) || typeof(T) == typeof(string) || typeof(K) == typeof(string))
+            {
+                double a = Convert.ToDouble(Value1);
+                double b = Convert.ToDouble(Value2);
+                Console.WriteLine($"{Value1} - {Value2} = {a - b}");
+            }
+            else { Console.WriteLine("Неверный тип"); }
+        }
+
+        public void Multiply()
+        {
+            if (typeof(T) == typeof(int) || typeof(K) == typeof(int) || typeof(T) == typeof(double) || typeof(K) == typeof(double) || typeof(T) == typeof(string) || typeof(K) == typeof(string))
+            {
+                double a = Convert.ToDouble(Value1);
+                double b = Convert.ToDouble(Value2);
+                Console.WriteLine($"{Value1} * {Value2} = {a * b}");
+            }
+            else { Console.WriteLine("Неверный тип"); }
+        }
+
+        public void Divide()
+        {
+            if (typeof(T) == typeof(int) || typeof(K) == typeof(int) || typeof(T) == typeof(double) || typeof(K) == typeof(double) || typeof(T) == typeof(string) || typeof(K) == typeof(string))
+            {
+                double a = Convert.ToDouble(Value1);
+                double b = Convert.ToDouble(Value2);
+                Console.WriteLine($"{Value1} / {Value2} = {a / b}");
+            }
+            else { Console.WriteLine("Неверный тип"); }
+        }
+    }
+}
---
+++ T_20 (Tasks)/Hometask/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Hometask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WriteLine("Объект №1\n");
+                Calculator<int, int> calculator = new Calculator<int, int>(1, 2);
+                calculator.Add();
+                calculator.Substract();
+                calculator.Multiply();
+                calculator.Divide();
+
+                WriteLine("\nОбъект №2\n");
+                Calculator<int, double> calculator1 = new Calculator<int, double>(2, 4);
+                calculator1.Add();
+                calculator1.Substract();
+                calculator1.Multiply();
+                calculator1.Divide();
+
+                WriteLine("\nОбъект №3\n");
+                Calculator<int, string> calculator2 = new Calculator<int, string>(3, "5");
+                calculator2.Add();
+                calculator2.Substract();
+                calculator2.Multiply();
+                calculator2.Divide();
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Исключение: {ex.Message}");
+                WriteLine($"Метод: {ex.TargetSite}");
+                WriteLine($"Трассировка стека: {ex.StackTrace}");
+            }
+            ReadKey();
+        }
+    }
+}
