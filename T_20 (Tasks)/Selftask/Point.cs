1e66622602ea87e68e8c4340e08e2727d0c0a030
Kapebah <sh1n0sh1@mail.ru>
2024-04-25 21:23:03 +0300

    selftask

---
+++ T_20 (Tasks)/Selftask/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Selftask
+{
+    class Point<T, K>
+    {
+        public T X { get; set; }
+        public K Y { get; set; }
+        public Point() { }
+        public Point(T x, K y) {  X = x; Y = y; }
+        public int Output()
+        {
+            WriteLine($"X = {X}\t\tY = {Y}");
+            return 1;
+        }
+    }
+}
---
+++ T_20 (Tasks)/Selftask/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Selftask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                WriteLine("Объект №1 с целым типом");
+                Point<int, int> ints = new Point<int, int>(5, 7);
+                ints.Output();
+                WriteLine();
+
+                WriteLine("Объект №1 с вещественным типом");
+                Point<double, double> doubles = new Point<double, double>(6.3, 9.8);
+                doubles.Output();
+                WriteLine();
+
+                WriteLine("Объект №1 со строковым типом");
+                Point<string, string> strings = new Point<string, string>("98", "32");
+                strings.Output();
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"Исключение: {ex.Message}");
+                WriteLine($"Метод: {ex.TargetSite}");
+                WriteLine($"Трассировка стека: {ex.StackTrace}");
+            }
+            ReadKey();
+        }
+    }
+}
